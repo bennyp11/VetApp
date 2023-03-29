67c0643bc2bdbf03c2783e105c34592e38921cd7
bennyp11 <ben.r.pollock@gmail.com>
2023-03-29 16:58:05 -0700

    made a model for customers

---
+++ src/main/java/com/vetapp/demo/Models/Customers.java
@@ -0,0 +1,88 @@
+package com.vetapp.demo.Models;
+
+
+public class Customers {
+    
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String[] petNames;
+    private String address;
+    private int visits;
+    
+    public Customers(String name, String email, String phoneNumber, String[] petNames, String address, int visits) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.petNames = petNames;
+        this.address = address;
+        this.visits = visits;
+    }
+    
+    // Getters and setters for all instance variables
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String[] getPetNames() {
+        return petNames;
+    }
+    
+    public void setPetNames(String[] petNames) {
+        this.petNames = petNames;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public int getVisits() {
+        return visits;
+    }
+    
+    public void setVisits(int visits) {
+        this.visits = visits;
+    }
+    
+    // toString() method for printing customer details
+    
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Name: " + name + "\n");
+        sb.append("Email: " + email + "\n");
+        sb.append("Phone number: " + phoneNumber + "\n");
+        sb.append("Pet names: ");
+        for (String petName : petNames) {
+            sb.append(petName + ", ");
+        }
+        sb.append("\n");
+        sb.append("Address: " + address.toString() + "\n");
+        sb.append("Visits: " + visits);
+        return sb.toString();
+    }
+}
