7232923068aee88c485bad82cf42c068c5736357
bennyp11 <ben.r.pollock@gmail.com>
2023-05-01 20:20:00 -0700

    adding SignUpDAO.java

--- src/main/java/com/vetapp/demo/DAO/SignUpDAO.java
+++ src/main/java/com/vetapp/demo/DAO/SignUpDAO.java
@@ -1,5 +1,30 @@
 package com.vetapp.demo.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class SignUpDAO {
 
+    private Connection connection;
+
+    public SignUpDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean registerUser(String username, String email, String password) {
+        String query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, password);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            System.err.println("Error registering user: " + e.getMessage());
+            return false;
+        }
+    }
 }
