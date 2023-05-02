f1cf41bf0edac2973ed96d3a8f6184b4cfb50116
bennyp11 <ben.r.pollock@gmail.com>
2023-05-01 20:42:15 -0700

    updated post mappings & model and view for SignUpController.java

--- src/main/java/com/vetapp/demo/Controllers/SignUpController.java
+++ src/main/java/com/vetapp/demo/Controllers/SignUpController.java
@@ -1,43 +1,37 @@
 package com.vetapp.demo.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 import com.vetapp.demo.Config.DB;
 import com.vetapp.demo.DAO.SignUpDAO;
 import com.vetapp.demo.Models.Customers;
 
+@Controller
 public class SignUpController {
 
     private SignUpDAO signUpDAO;
 
+    @Autowired
     public SignUpController(DB db) {
         signUpDAO = new SignUpDAO(db);
     }
 
-    public boolean registerCustomer(Customers customer) {
-        return signUpDAO.registerCustomer(customer);
-    }
-
-    public static void main(String[] args) {
-        DB db = new DB();
-        db.init();
-        SignUpController signUpController = new SignUpController(db);
-
-        // Example usage
-        String name = "John Doe";
-        String email = "john.doe@example.com";
-        String phoneNumber = "123-456-7890";
-        String[] petNames = {"Buddy", "Max"};
-        String address = "1234 Elm Street";
-        int visits = 0;
-        String password = "password123";
-
-        Customers customer = new Customers(name, email, password, phoneNumber, petNames, address, visits);
-
-        if (signUpController.registerCustomer(customer)) {
-            System.out.println("Customer registration successful!");
+    @PostMapping("/sign-up")
+    public ModelAndView registerCustomer(@ModelAttribute Customers customer) {
+        ModelAndView modelAndView = new ModelAndView();
+        
+        if (signUpDAO.registerCustomer(customer)) {
+            modelAndView.setViewName("welcome");
+            modelAndView.addObject("message", "Customer registration successful!");
         } else {
-            System.out.println("Customer registration failed.");
+            modelAndView.setViewName("sign-up");
+            modelAndView.addObject("error", "Customer registration failed. Please try again.");
         }
 
-        db.destroy();
+        return modelAndView;
     }
 }
