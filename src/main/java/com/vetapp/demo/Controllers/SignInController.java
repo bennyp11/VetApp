3d03e60325bd2cbe7fbb96cb6e4abda9746ea6ed
bennyp11 <ben.r.pollock@gmail.com>
2023-05-01 20:40:30 -0700

    updated SignInController.java

--- src/main/java/com/vetapp/demo/Controllers/SignInController.java
+++ src/main/java/com/vetapp/demo/Controllers/SignInController.java
@@ -1,41 +1,39 @@
 package com.vetapp.demo.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.vetapp.demo.Config.DB;
 import com.vetapp.demo.DAO.SignInDAO;
 
+@Controller
 public class SignInController {
 
     private SignInDAO signInDAO;
 
+    @Autowired
     public SignInController(DB db) {
         this.signInDAO = new SignInDAO(db);
     }
 
-    public boolean authenticateCustomer(String email, String password) {
-        return signInDAO.authenticateCustomer(email, password);
-    }
-
-    public static void main(String[] args) {
-        DB db = new DB();
-        db.init();
-        SignInController signInController = new SignInController(db);
-
-        // Example usage
-        String email = "john.doe@example.com";
-        String password = "password123";
-
-        if (signInController.authenticateCustomer(email, password)) {
-            System.out.println("Customer authentication successful!");
+    @PostMapping("/sign-in")
+    public ModelAndView authenticateCustomer(@RequestParam("email") String email, @RequestParam("password") String password) {
+        ModelAndView modelAndView = new ModelAndView();
+        
+        if (signInDAO.authenticateCustomer(email, password)) {
+            modelAndView.setViewName("welcome");
+            modelAndView.addObject("message", "Customer authentication successful!");
         } else {
-            System.out.println("Customer authentication failed.");
+            modelAndView.setViewName("sign-in");
+            modelAndView.addObject("error", "Customer authentication failed. Please try again.");
         }
 
-        db.destroy();
+        return modelAndView;
     }
 }
+
 
 
